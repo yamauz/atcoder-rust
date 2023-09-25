737e3e8bc136bf046fe561fa1bb71c5485f0a2ef
yamauz <d-yamauchi@hitotocolabo.co.jp>
2023-09-25 23:22:24 +0900

    wip

---
+++ abc269/src/bin/a.rs
@@ -0,0 +1,12 @@
+use proconio::input;
+fn main() {
+    input! {
+       a:isize,
+       b:isize,
+       c:isize,
+       d:isize,
+    }
+
+    println!("{}", (a + b) * (c - d));
+    println!("Takahashi");
+}
