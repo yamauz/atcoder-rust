03dfb1560ba60b4e29a30c7aa5ea207b561abc54
yamauz <d-yamauchi@hitotocolabo.co.jp>
2023-12-17 15:45:25 +0900

    abc319-a

--- abc319/src/bin/a.rs
+++ abc319/src/bin/a.rs
@@ -1,3 +1,25 @@
+use std::collections::HashMap;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        s: String,
+    }
+
+    let mut list = HashMap::new();
+    list.insert(String::from("tourist"), 3858);
+    list.insert(String::from("ksun48"), 3679);
+    list.insert(String::from("Benq"), 3658);
+    list.insert(String::from("Um_nik"), 3648);
+    list.insert(String::from("apiad"), 3638);
+    list.insert(String::from("Stonefeang"), 3630);
+    list.insert(String::from("ecnerwala"), 3613);
+    list.insert(String::from("mnbvmar"), 3555);
+    list.insert(String::from("newbiedmy"), 3516);
+    list.insert(String::from("semiexp"), 3481);
+
+    let ans = list.get(&s).unwrap();
+
+    println!("{}", ans);
 }
