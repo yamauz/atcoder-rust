e06348c1cbb6099e951466bddebfead0d88da67a
yamauz <d-yamauchi@hitotocolabo.co.jp>
2023-12-18 21:47:07 +0900

    abc321-a

--- abc321/src/bin/a.rs
+++ abc321/src/bin/a.rs
@@ -1,3 +1,17 @@
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        n: Chars
+    }
+
+    if n.iter()
+        .tuple_windows()
+        .all(|(a, b)| a.to_string().parse::<i32>().unwrap() > b.to_string().parse::<i32>().unwrap())
+    {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
