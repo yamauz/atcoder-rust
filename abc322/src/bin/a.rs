bf0dee4bc1be376b481fff28e00c3438c3692984
yamauz <d-yamauchi@hitotocolabo.co.jp>
2023-12-18 22:01:49 +0900

    abc322-a

--- abc322/src/bin/a.rs
+++ abc322/src/bin/a.rs
@@ -1,3 +1,17 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        _: usize,
+        n: Chars,
+    }
+
+    for (i, v) in n.windows(3).enumerate() {
+        if v[0] == 'A' && v[1] == 'B' && v[2] == 'C' {
+            println!("{}", i + 1);
+            return;
+        }
+    }
+
+    println!("{}", -1);
 }
