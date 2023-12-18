3b0142bfd0d26ad908a6a3f6d30082e07a19b3ab
yamauz <d-yamauchi@hitotocolabo.co.jp>
2023-12-18 22:47:28 +0900

    abc323-a

--- abc323/src/bin/a.rs
+++ abc323/src/bin/a.rs
@@ -1,3 +1,21 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    let pos = s
+        .iter()
+        .enumerate()
+        .position(|(i, &v)| (i + 1) % 2 == 0 && v == '1');
+
+    if let Some(i) = pos {
+        if s[i] == '1' {
+            println!("No");
+            return;
+        }
+    }
+
+    println!("Yes");
 }
