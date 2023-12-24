b5cae8d8b4740ce0728a898f6ab5a9056f5f248e
yamauz <d-yamauchi@hitotocolabo.co.jp>
2023-12-24 17:44:47 +0900

    abc327-a

--- abc327/src/bin/a.rs
+++ abc327/src/bin/a.rs
@@ -1,3 +1,17 @@
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        _: usize,
+        s: Chars,
+    }
+
+    for (&x, &y) in s.iter().tuple_windows() {
+        if (x == 'a' && y == 'b') || (x == 'b' && y == 'a') {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
