ed82f1a6840e90549c1ce640a13e87d01598b321
yamauz <d-yamauchi@hitotocolabo.co.jp>
2023-12-24 17:53:57 +0900

    abc330-a

--- abc330/src/bin/a.rs
+++ abc330/src/bin/a.rs
@@ -1,3 +1,13 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        l: usize,
+        a: [usize; n],
+    }
+
+    let ans = a.iter().filter(|&&x| x >= l).count();
+
+    println!("{}", ans);
 }
