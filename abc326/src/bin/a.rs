6d1e657129f08a93ae76eefc0129b976af9178d9
yamauz <d-yamauchi@hitotocolabo.co.jp>
2023-12-24 17:19:35 +0900

    abc326-a

--- abc326/src/bin/a.rs
+++ abc326/src/bin/a.rs
@@ -1,3 +1,14 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: isize,
+        b: isize,
+    }
+
+    if (a > b && a - b <= 3) || (a < b && b - a <= 2) {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
