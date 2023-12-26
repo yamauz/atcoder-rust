fb0584e22a97a7155348142c7bb2a19df90d5d7e
yamauz <d-yamauchi@hitotocolabo.co.jp>
2023-12-26 23:28:25 +0900

    abc331-a

--- abc331/src/bin/a.rs
+++ abc331/src/bin/a.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        month: usize,
+        day: usize,
+        mut y: usize,
+        mut m: usize,
+        mut d:usize
+    }
+
+    if day == d {
+        d = 1;
+        m += 1;
+    } else {
+        d += 1;
+        println!("{} {} {}", y, m, d);
+        return;
+    }
+
+    if m > month {
+        m = 1;
+        y += 1;
+    }
+    println!("{} {} {}", y, m, d);
 }
