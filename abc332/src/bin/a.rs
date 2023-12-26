536b0c7c556bbe22e1a13804dfe59c5f798b37a1
yamauz <d-yamauchi@hitotocolabo.co.jp>
2023-12-26 23:36:05 +0900

    abc332-a

--- abc332/src/bin/a.rs
+++ abc332/src/bin/a.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: usize,
+        k: usize,
+        pq: [(usize,usize); n],
+    }
+
+    let sum = pq.iter().map(|(p, q)| p * q).sum::<usize>();
+
+    if sum >= s {
+        println!("{}", sum);
+    } else {
+        println!("{}", sum + k);
+    }
 }
